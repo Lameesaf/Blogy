8e0ecbd5c62d37cfb5d1de92472f6ffad8916573
Lameesaf <lafallaj2@gmail.com>
2019-12-07 18:31:21 +0300

    add comment schema

--- app/models/article.js
+++ app/models/article.js
@@ -1,17 +1,25 @@
 //Require necessary NPM packags
 const mongoose = require('mongoose');
 
+
+const commentSchema = new mongoose.Schema({
+  commentText: String
+});
+
+
 //Define Article Schema
 const articleSchema = new mongoose.Schema({
 title: {type: String, required:true},
 content:  String,
 author: {type: String, required: true},
 published: {type: Boolean, default:true},
-publishedOn: {type:Date, default: Date.now}
+publishedOn: {type:Date, default: Date.now},
+comments: [commentSchema]
 },{timestamps: true});
 
 //Compile our model based on schema
 const Article = mongoose.model('Articla', articleSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
 //export model for use
-module.exports = Article;
+module.exports ={ Article, Comment};
